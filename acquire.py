223f666c2c69f17d20f826be226a6eb04900e4ba
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-05-27 16:27:59 -0500

    acquire upadtes

---
+++ acquire.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import os
+from env import host, user, password
+
+
+### *** acquire Telco data *****
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+    
+
+# Use the above helper function and a sql query in a single function.
+def new_telco_data():
+    '''
+    This function reads data from the Codeup db into a df.
+    '''
+    telco_sql = "SELECT * \
+                 FROM customers \
+                 JOIN contract_types USING(contract_type_id) \
+                 JOIN internet_service_types USING(internet_service_type_id)\
+                 JOIN payment_types USING(payment_type_id);"    
+    return pd.read_sql(telco_sql, get_connection('telco_churn'))
+
+def get_telco_data(cached=False):
+    '''
+    This function reads in telco churn data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in telco df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('telco_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = new_telco_data()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('telco_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('telco_df.csv', index_col=0)
+        
+    return df
