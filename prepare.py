8747ed36eb66215397783c5888d273d3d60249ff
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-05-29 22:33:55 -0500

    updates

---
+++ prepare.py
@@ -0,0 +1,13 @@
+# include train/validate/test functions
+
+
+# Include code for missing values
+
+
+# encode variables as needed
+
+
+# create any new features if necessary 
+
+
+# df = df.loc[:, ~df.columns.duplicated()]
